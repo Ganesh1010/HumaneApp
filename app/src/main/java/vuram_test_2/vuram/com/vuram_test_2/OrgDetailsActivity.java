83e489a19be1aa3891ea16c16372e00d9b4c3ce4
akshayagr <akshayagr@vuram.com>
2017-05-26 18:51:10 +0530

    Coordinator registration page fragment completed

--- app/src/main/java/vuram_test_2/vuram/com/vuram_test_2/OrgDetailsActivity.java
+++ app/src/main/java/vuram_test_2/vuram/com/vuram_test_2/OrgDetailsActivity.java
@@ -1,6 +1,7 @@
 package vuram_test_2.vuram.com.vuram_test_2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
