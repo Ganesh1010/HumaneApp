b6760270c6ccd13ddf27b93e164ad44132728bfc
sujithv <sujithv@vuram.com>
2017-06-09 11:23:37 +0530

    DonationListAdapter is added to display the recycler view of donation details

---
+++ app/src/main/java/vuram_test_2/vuram/com/vuram_test_2/DonationListAdapter.java
@@ -0,0 +1,36 @@
+package vuram_test_2.vuram.com.vuram_test_2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sujithv on 6/9/2017.
+ */
+
+public class DonationListAdapter extends RecyclerView.Adapter {
+
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
